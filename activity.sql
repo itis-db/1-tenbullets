7494892846adcb131817a426a694688579f5fb25
tenbullets <lnayc368@gmail.com>
2023-10-24 11:36:29 +0300

    added code

--- activity.sql
+++ activity.sql
@@ -1,15 +1,33 @@
--- рекурсия 
-WITH RECURSIVE r AS (
-    SELECT 
-        1 AS i, 
-        1 AS factorial
-    
-    UNION 
+WITH RECURSIVE HierarchicalActivity AS (
 
-    SELECT 
-        i+1 AS i, 
-        factorial * (i+1) as factorial 
-    FROM r
-    WHERE i < 10
+    SELECT
+        A.ActivityId,
+        A.Name AS ActivityName,
+        A.AreaId,
+        AT.Name AS ActivityTypeName,
+        1 AS Level
+    FROM Activity A
+    JOIN ActivityType AT ON A.ActivityTypeId = AT.ActivityTypeId
+    WHERE A.ParentId IS NULL
+
+
+    UNION ALL
+
+    SELECT
+        A.ActivityId,
+        A.Name AS ActivityName,
+        A.AreaId,
+        AT.Name AS ActivityTypeName,
+        HA.Level + 1
+    FROM Activity A
+    JOIN ActivityType AT ON A.ActivityTypeId = AT.ActivityTypeId
+    JOIN HierarchicalActivity HA ON A.ParentId = HA.ActivityId
+    
 )
-SELECT * FROM r;
+
+SELECT
+    HA.ActivityName,
+    HA.AreaId AS Territory,
+    HA.ActivityTypeName
+FROM HierarchicalActivity HA
+WHERE HA.Level IN (3, 5, 7);
--- data.sql
+++ data.sql
@@ -1,17 +1,34 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+INSERT INTO ActivityType (ActivityTypeId, Name)
+VALUES
+    (1, 'Program'),
+    (2, 'Sub Program'),
+    (3, 'Project'),
+    (4, 'Contract'),
+    (5, 'Point');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId, Name, AreaId)
+VALUES
+    (1, NULL, 1, 'Program 1', NULL),
+    (2, NULL, 1, 'Program 2', NULL),
+    (3, 1, 2, 'Subprogram 1', NULL),
+    (4, 1, 2, 'Subprogram 2', NULL),
+    (5, 3, 3, 'Project 1', NULL),
+    (6, 3, 3, 'Project 2', NULL),
+    (7, 5, 5, 'КТ', NULL);
+
+INSERT INTO Program (ActivityId)
+VALUES (1),(2),(3),(4);
+
+INSERT INTO SubProgram (ActivityId)
+VALUES (3),(4);
+
+INSERT INTO Project (ActivityId)
+VALUES (7),(8),(9),(10);
+
+INSERT INTO Point (ActivityId)
+VALUES (1),(2),(3),(4),(5),(6);
+
+INSERT INTO Contract (ActivityId, AreaId)
+VALUES (3, 2),(9, 2),(7, 4);
+
+
